2fe8f245153fa84d887d2c4b98a90f4d3dfabeb7
Iain Shorter <iain.shorter@nanoporetech.com>
2023-07-19 14:21:56 +0100

    Simplify token types and switch index based positional data

--- src/scanner/Position.type.ts
+++ src/scanner/Position.type.ts
@@ -2,3 +2,9 @@ export interface Position {
   column: number;
   row: number;
 }
+
+export interface Fragment {
+  start: number;
+  end: number;
+  source: string[];
+}
--- src/scanner/token.type.ts
+++ src/scanner/token.type.ts
@@ -1,27 +1,10 @@
-import type { Position } from './Position.type';
+import type { Fragment } from './Position.type';
 
 export type TokenTypes = 'identifier' | 'symbol' | 'number' | 'string';
 
 export interface Token {
   readonly type: TokenTypes;
-  readonly start: Position;
-  readonly end: Position;
+  readonly row: number;
+  readonly fragment: Fragment;
   readonly value: string;
 }
-
-export interface IdentifierToken extends Token {
-  readonly type: 'identifier';
-}
-
-export interface SymbolToken extends Token {
-  readonly type: 'symbol';
-}
-
-export interface NumberToken extends Token {
-  readonly type: 'number';
-}
-
-export interface StringToken extends Token {
-  readonly type: 'string';
-}
-
